08c29b3be4dc95e4cabb7ce9dd526f0af4fb63c3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-23 04:05:00 +0600

    complete auth controller functionality

--- 12-Attendance-System-RA/backend/controller/auth.js
+++ 12-Attendance-System-RA/backend/controller/auth.js
@@ -1,3 +1,5 @@
+const { registerService, loginService } = require("../service/auth");
+
 const registerController = async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -7,7 +9,8 @@ const registerController = async (req, res, next) => {
   }
 
   try {
-
+    const user = await registerService({ name, email, password });
+    return res.status(201).json({ message: "User created successfully", user });
   } catch (e) {
     next(e);
   }
@@ -17,7 +20,8 @@ const loginController = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-
+    const token = await loginService({ email, password });
+    return res.status(200).json({ message: "Login Successful", token });
   } catch (e) {
     next(e);
   }
