bc491bebc7cce09eeec3a409f16e153cd4feea0e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-08 16:12:18 +0600

    fibonacci take huge time

--- 05-Array-Operations-Imperative-vs-Declarative/array2.js
+++ 05-Array-Operations-Imperative-vs-Declarative/array2.js
@@ -35,4 +35,14 @@ for (let i = 0; i < arr.length; i++) {
     newArr.push(arr[i]);
   }
 }
-console.log(newArr);
+// console.log(newArr);
+
+function fib(n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+// console.log(fib(55));
