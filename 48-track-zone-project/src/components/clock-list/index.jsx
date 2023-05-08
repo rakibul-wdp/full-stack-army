8f57bcb12a4ed79b4d351f04ca13e9e01c73e236
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-09 04:25:00 +0600

    recive props and pass by click list item in clock list component

--- 48-track-zone-project/src/components/clock-list/index.jsx
+++ 48-track-zone-project/src/components/clock-list/index.jsx
@@ -1,5 +1,27 @@
-const ClockList = () => {
-  return <div>ClockList</div>;
+import ClockListItem from "./clock-list-item";
+
+const ClockList = ({ clocks, updateClock, deleteClock, localClock }) => {
+  return (
+    <div>
+      <h3>Other Clocks</h3>
+      <hr />
+      {ClockList.length === 0 ? (
+        <p>There is no clock, please create one</p>
+      ) : (
+        <div>
+          {clocks.map((clock) => (
+            <ClockListItem
+              key={clock.id}
+              clock={clock}
+              updateClock={updateClock}
+              deleteClock={deleteClock}
+              localClock={localClock}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ClockList;
