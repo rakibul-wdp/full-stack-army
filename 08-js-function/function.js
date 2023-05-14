612072c56365b92de7f553ff96d5068aa4730044
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-14 17:36:18 +0600

    create all nessary and one parant function

--- 08-js-function/function.js
+++ 08-js-function/function.js
@@ -8,3 +8,45 @@ function nameOfTheFunction(/** input something */) {
 // There are two steps
 // - Define a function
 // - Invoke a function
+
+function testFunction(a, b) {
+  const result = a + b + Math.max(a, b);
+  // console.log(result);
+}
+
+testFunction(10, 20);
+
+function jobholderLifecycle(name) {
+  awake(name);
+  eat(name, "breakfast");
+  walk(name, "office");
+  work(name);
+  eat(name, "Lunch");
+  walk(name, "home");
+  eat(name, "dinner");
+  sleep(name);
+}
+
+function sleep(name) {
+  console.log(`${name} is sleeping`);
+}
+
+function awake(name) {
+  console.log(`${name} is awake`);
+}
+
+function eat(name, time) {
+  console.log(`${name} is taking ${time}`);
+}
+
+function walk(name, destination) {
+  console.log(`${name} is walking to ${destination}`);
+}
+
+function study(name) {
+  console.log(`${name} is studying`);
+}
+
+function work(name) {
+  console.log(`${name} is working`);
+}
