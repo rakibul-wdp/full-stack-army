0aa72da9938d0eb8cf6e8bca53adcc4374dd73da
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-14 13:52:24 +0600

    function boilarplate

---
+++ 08-js-function/function.js
@@ -0,0 +1,10 @@
+function nameOfTheFunction(/** input something */) {
+  // function body
+  // you can write any valid js code here
+
+  // return a result
+}
+
+// There are two steps
+// - Define a function
+// - Invoke a function
