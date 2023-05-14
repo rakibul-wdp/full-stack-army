499c08d085e63e216790cc043a77b901dbf08f50
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-14 19:28:57 +0600

    function composition

--- 08-js-function/advanceFunction.js
+++ 08-js-function/advanceFunction.js
@@ -1,11 +1,21 @@
 function sum(a, b) {
-  console.log(a + b);
+  return a + b;
 }
 
+// G(a, b)
 function sub(a, b) {
-  console.log(a - b);
+  return a - b;
 }
 
 function times(a, b) {
-  console.log(a * b);
+  return a * b;
 }
+
+const a = 10;
+const b = 20;
+
+const r = Math.abs(times(sum(a, b), sub(a, b)));
+console.log(r);
+
+// Match.abs(n) -> F
+F(G(10, 20))
