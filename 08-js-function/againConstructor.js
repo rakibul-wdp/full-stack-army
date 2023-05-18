4c377b2f178ceb6863d1b052ba0003dc40b2947e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-18 09:27:05 +0600

    use new function

--- 08-js-function/againConstructor.js
+++ 08-js-function/againConstructor.js
@@ -13,3 +13,26 @@ console.log(typeof greetFn);
 // console.log(greetFn.toString());
 const fName = greetFn("Abul Babul", "abulbabul@gmail.com");
 console.log("First Name", fName);
+
+const operations = [
+  {
+    args: [10, 20],
+    params: ["a", "b"],
+    body: "console.log('a + b =', a + b)"
+  },
+  {
+    args: [10, 20],
+    params: ["a", "b"],
+    body: "console.log('a - b =', a - b)"
+  },
+  {
+    args: [10, 20],
+    params: ["a", "b"],
+    body: "console.log('a * b =', a * b)"
+  }
+]
+
+operations.forEach((operation) => {
+  const fn = new Function(...operation.params, operation.body);
+  fn(...operation.args);
+})
