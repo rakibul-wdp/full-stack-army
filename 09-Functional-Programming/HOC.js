eea42c2343b3d80ee482710b32ef0479dde17cd8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-20 15:48:50 +0600

    hoc function example

--- 09-Functional-Programming/HOC.js
+++ 09-Functional-Programming/HOC.js
@@ -11,3 +11,41 @@
  * - function context
  * - hoisting
  */
+
+function generateTwoRandNumber(max, cb) {
+  const random1 = Math.floor(Math.random() * max); // const
+  const random2 = Math.floor(Math.random() * max); // const
+
+  const result = cb(random1, random2);
+  return result;
+}
+
+generateTwoRandNumber(100, (rand1, rand2) => {
+  console.log(rand1, rand2);
+});
+
+console.log(generateTwoRandNumber(1000, (rand1, rand2) => rand1 + rand2));
+console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 - rand2));
+console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand2));
+console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand1 + rand2 * rand2));
+
+// function randomSum(max) {
+//   const random1 = Math.floor(Math.random() * max);
+//   const random2 = Math.floor(Math.random() * max);
+
+//   return random1 + random2; // placeholder
+// }
+
+// function randomSub(max) {
+//   const random1 = Math.floor(Math.random() * max);
+//   const random2 = Math.floor(Math.random() * max);
+
+//   return random1 - random2; // placeholder
+// }
+
+// function randomSqrSum(max) {
+//   const random1 = Math.floor(Math.random() * max);
+//   const random2 = Math.floor(Math.random() * max);
+
+//   return random1 * random1 + random2 * random2; // placeholder
+// }
