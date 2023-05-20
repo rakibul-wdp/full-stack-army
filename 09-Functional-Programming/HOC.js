a0d6bdd1653902cf8ea199623d92c0f243cbb896
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-20 16:50:00 +0600

    scope

--- 09-Functional-Programming/HOC.js
+++ 09-Functional-Programming/HOC.js
@@ -21,13 +21,13 @@ function generateTwoRandNumber(max, cb) {
 }
 
 generateTwoRandNumber(100, (rand1, rand2) => {
-  console.log(rand1, rand2);
+  // console.log(rand1, rand2);
 });
 
-console.log(generateTwoRandNumber(1000, (rand1, rand2) => rand1 + rand2));
-console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 - rand2));
-console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand2));
-console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand1 + rand2 * rand2));
+// console.log(generateTwoRandNumber(1000, (rand1, rand2) => rand1 + rand2));
+// console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 - rand2));
+// console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand2));
+// console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand1 + rand2 * rand2));
 
 // function randomSum(max) {
 //   const random1 = Math.floor(Math.random() * max);
@@ -49,3 +49,52 @@ console.log(generateTwoRandNumber(10, (rand1, rand2) => rand1 * rand1 + rand2 * 
 
 //   return random1 * random1 + random2 * random2; // placeholder
 // }
+
+// function sqr(n) {
+//   return n * n;
+// }
+
+// function cube(n) {
+//   return n * n * n;
+// }
+
+function power(p) {
+  return function (n) {
+    let result = 1;
+    for (let i = 1; i <= p; i++) {
+      result *= n;
+    }
+    return result;
+  }
+}
+
+const sqr = power(2);
+const cube = power(3);
+const power8 = power(8);
+
+console.log("SQR", sqr);
+console.log("Cube", cube);
+console.log("Power 8", power8);
+
+console.log(power8(3));
+console.log(sqr(4));
+console.log(sqr(5));
+
+// lexing / parsing / tokenize -> compile -> run
+
+// global
+// local
+// block scope
+
+const a = 10;
+function mostOuter() {
+  function outer() {
+    console.log(a);
+  }
+}
+
+{
+  const notScoped = "not scoped"
+}
+
+console.log(notScoped);
