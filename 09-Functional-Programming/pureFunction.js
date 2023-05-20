353c2ad64f504dafbbc83cab9f84984947ed724b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-20 14:55:35 +0600

    pure impure function

--- 09-Functional-Programming/pureFunction.js
+++ 09-Functional-Programming/pureFunction.js
@@ -5,11 +5,23 @@ function sum(a, b) {
 
 sum(10, 20); // 30
 
-// side effect
-let limit = 100;
-function changeLimit() {
-  limit = 500;
+// // side effect
+// let limit = 100;
+// function changeLimit() {
+//   limit = 500;
+// }
+
+// changeLimit(limit);
+// console.log(limit);
+
+// pure function
+const arr = [1, 2, 3];
+function add(arr, data) {
+  arr.push(data);
 }
 
-changeLimit(limit);
-console.log(limit);
+// impure function
+const arr2 = [1, 2, 3];
+function add(data) {
+  arr.push(data);
+}
