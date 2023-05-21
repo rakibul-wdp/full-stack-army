e05310c768e91562ac96f726f12c73953eb5dd04
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-21 14:14:34 +0600

    iterator concept with example

--- 11-Async-Iterator&Generator/iterator.js
+++ 11-Async-Iterator&Generator/iterator.js
@@ -1,21 +1,26 @@
-const channel = "Abul";
-const channelIterator = channel[Symbol.iterator]();
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-// console.log(channelIterator.next());
-
-for (let v of channel) {
-  // console.log(v);
+const range = {
+  start: 0,
+  stop: 100,
+  step: 5
 }
 
-while (true) {
-  const data = channelIterator.next();
-  if (data.done) {
-    break;
+range[Symbol.iterator] = function () {
+  let current = this.start;
+  const stop = this.stop;
+  const step = this.step;
+
+  return {
+    next() {
+      const o = {
+        value: current,
+        done: current > stop,
+      };
+      current += step;
+      return o;
+    }
   }
-  console.log(data.value);
+}
+
+for (let v of range) {
+  console.log(v);
 }
