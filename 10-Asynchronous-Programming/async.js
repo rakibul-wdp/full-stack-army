1d7e7d4e98799af179376dcc76c2d5069b8ea896
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-21 08:42:33 +0600

    user data rquirement anylaices

--- 10-Asynchronous-Programming/async.js
+++ 10-Asynchronous-Programming/async.js
@@ -1,17 +1,34 @@
-function main() {
-  setTimeout(() => {
-    console.log("load last")
-  }, 10)
+// function main() {
+//   setTimeout(() => {
+//     console.log("load last")
+//   }, 10)
 
-  setTimeout(() => {
-    console.log("load first")
-  }, 0)
+//   setTimeout(() => {
+//     console.log("load first")
+//   }, 0)
 
-  test()
-}
+//   test()
+// }
 
-function test() {
-  console.log("test")
-}
+// function test() {
+//   console.log("test")
+// }
 
-main();
+// main();
+
+/**
+ * 1. find user by username
+ * 2. find posts by userId
+ * 3. find latest post
+ * 4. find comments by post id
+ * 5. find latest comment
+ * 6. find username of the last commented user
+ */
+
+/**
+ * /users?username=[username]
+ * /posts?userId=[userId]
+ * /comments?postsId-[postId
+ * /users?username=[username]
+ */
+
