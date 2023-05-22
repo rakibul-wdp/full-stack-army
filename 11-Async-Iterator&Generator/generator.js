07581b7c806bdc0093e86d52cf6de0de237ab62f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-22 10:46:51 +0600

    yield

--- 11-Async-Iterator&Generator/generator.js
+++ 11-Async-Iterator&Generator/generator.js
@@ -26,5 +26,26 @@ const rangeIt = range(1, 10, 3);
 // console.log(rangeIt.next());
 
 for (let v of range()) {
-  console.log(v);
+  // console.log(v);
 }
+
+function* generateId() {
+  let index = 1;
+  while (true) {
+    yield index++
+  }
+}
+
+const generateUserId = generateId();
+const generateProductId = generateId();
+
+console.log("User", generateUserId.next().value);
+console.log("User", generateUserId.next().value);
+console.log("User", generateUserId.next().value);
+
+console.log("Product", generateProductId.next().value);
+console.log("Product", generateProductId.next().value);
+console.log("Product", generateProductId.next().value);
+console.log("Product", generateProductId.next().value);
+console.log("Product", generateProductId.next().value);
+console.log("Product", generateProductId.next().value);
