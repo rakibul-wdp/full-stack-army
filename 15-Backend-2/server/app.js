8ec9904f3e705afa8b4bfb51f6e309cb94d5bea8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-27 10:16:28 +0600

    access public index file by static

--- 15-Backend-2/server/app.js
+++ 15-Backend-2/server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 const simpleLogger = (req, res, next) => {
   console.log(`${req.url} - ${req.method} - ${new Date().toISOString()}`);
@@ -18,10 +18,6 @@ app.use(simpleLogger);
 app.get("/hello", (req, res) => {
   res.json({ message: "Hello" });
 })
-
-app.get("/", (req, res, next) => {
-  res.json({ message: "Sweet Home" });
-});
 
 app.listen(5000, () => {
   console.log("application is listening on port 5000");
