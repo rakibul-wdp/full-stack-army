c2becf3d7e592854a8e666d010b4fa6017c1287c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-28 11:52:36 +0600

    app use configaration

--- 15-Backend-2/backend/app/app.js
+++ 15-Backend-2/backend/app/app.js
@@ -1,6 +1,17 @@
+require("dotenv").config("../.env");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  [
+    morgan("dev"),
+    cors(),
+    express.json(),
+  ]
+);
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ message: "Success" });
