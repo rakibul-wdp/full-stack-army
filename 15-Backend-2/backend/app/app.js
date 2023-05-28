d004affe202d2eef8cd8672d59362c32d1e2af56
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-28 12:02:02 +0600

    global error handler

--- 15-Backend-2/backend/app/app.js
+++ 15-Backend-2/backend/app/app.js
@@ -14,7 +14,29 @@ app.use(
 );
 
 app.get("/health", (_req, res) => {
+  throw new Error("Error");
   res.status(200).json({ message: "Success" });
 });
+
+app.get("/health2", (_req, res) => {
+  throw new Error("Error");
+  res.status(200).json({ message: "Success" });
+});
+
+app.use((_req, _res, next) => {
+  const error = new Error("Resource Not Found");
+  error.status = 404
+  next(error)
+})
+
+app.use((error, _req, res, _next) => {
+  if (error.status) {
+    return res.status(error.status).json({
+      message: error.message
+    })
+  }
+
+  res.status(500).json({ message: "Something went wrong" });
+})
 
 module.exports = app;
