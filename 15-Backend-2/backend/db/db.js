a3f685bce4add867940f59839e8fc1bc458d666b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-31 08:21:34 +0600

    create all possible functionality

---
+++ 15-Backend-2/backend/db/db.js
@@ -0,0 +1,45 @@
+const Ticket = require("../models/Ticket");
+
+class MyDB {
+  constructor() {
+    this.ticket = [];
+  }
+
+  // create new ticket
+  create() {
+
+  }
+
+  // sell multiple ticket
+  bulkCreate() {
+
+  }
+
+  // return all tickets
+  find() {
+
+  }
+
+  // single ticket
+  findById() {
+
+  }
+
+  // update ticket info
+  updateById() {
+
+  }
+
+  // delete ticket from db
+  deleteById() {
+
+  }
+
+  draw() {
+
+  }
+}
+
+const myDB = new MyDB();
+
+module.exports = myDB;
