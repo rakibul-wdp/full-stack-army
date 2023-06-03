fbc4f18f3f0b6e00faaf21d5e621a8863ba94ab0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-03 16:02:33 +0600

    fix bug

--- 15-Backend-2/backend/db/db.js
+++ 15-Backend-2/backend/db/db.js
@@ -108,18 +108,29 @@ class MyDB {
    * @returns {Array<Ticket>}
    */
   draw(winnerCount) {
-    let indexes = new Array(winnerCount);
-    for (let i = 0; i < indexes.length; i++) {
-      let index = Math.floor(Math.random() * this.tickets.length);
-      while (indexes.includes(index)) {
-        index = Math.floor(Math.random() * this.tickets.length);
+    let winnerIndexes = new Array(winnerCount);
+
+    let index = 0;
+    while (index < winnerCount) {
+      let winnerIndex = Math.floor(Math.random() * this.tickets.length);
+      console.log("winner index", winnerIndex);
+      if (!winnerIndexes.includes(winnerIndex)) {
+        winnerIndexes[index++] = winnerIndex;
+        continue;
       }
-      indexes.push(index);
     }
 
-    console.log("indexes", indexes);
+    // for (let i = 0; i < indexes.length; i++) {
+    //   let index = Math.floor(Math.random() * this.tickets.length);
+    //   while (indexes.includes(index)) {
+    //     index = Math.floor(Math.random() * this.tickets.length);
+    //   }
+    //   indexes[i] = index;
+    // }
 
-    const winners = indexes.map((index) => this.tickets[index]);
+    // console.log("indexes", indexes);
+
+    const winners = winnerIndexes.map((index) => this.tickets[index]);
     return winners;
   }
 }
