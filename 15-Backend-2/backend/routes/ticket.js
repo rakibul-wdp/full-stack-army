c5e4f0c10468b98a6a2e23d055ceec46dc90d28c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-03 16:32:56 +0600

    create all nessary routes

--- 15-Backend-2/backend/routes/ticket.js
+++ 15-Backend-2/backend/routes/ticket.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
 const db = require("../db/db");
 
+router.get("/t/:ticketId", () => { });
+router.patch("/t/:ticketId", () => { });
+router.delete("/t/:ticketId", () => { });
+
+router.get("/u/:username", () => { });
+router.patch("/u/:username", () => { });
+router.delete("/u/:username", () => { });
+
+router.post("/sell", () => { });
+router.post("/bulk", () => { });
+router.get("/draw", () => { });
+router.get("", () => { });
+
+// router.route("/tickets/t/:ticketId").get(() => { }).patch(() => { }).delete(() => { });
+
 module.exports = router;
