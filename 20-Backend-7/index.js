a1ded61914a2043b40d3c4b83adc6c7323d144b9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-07 10:46:41 +0600

    create some person data in mongodb

--- 20-Backend-7/index.js
+++ 20-Backend-7/index.js
@@ -1,7 +1,29 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://doctor_practice:28LWDslaloONGvzu@cluster0.aee2j.mongodb.net/?retryWrites=true&w=majority').then(() => {
+const personSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  age: Number,
+  bio: String,
+  single: Boolean
+})
+
+const Person = mongoose.model("Person", personSchema);
+
+mongoose.connect('mongodb+srv://doctor_practice:28LWDslaloONGvzu@cluster0.aee2j.mongodb.net/?retryWrites=true&w=majority').then(async () => {
   console.log("Database Connected");
+  const person = new Person({
+    firstName: "Abul",
+    lastName: "Babul",
+    email: "abul@gmail.com",
+    age: 23,
+    bio: "I am a backend developer",
+    single: true,
+  });
+  await person.save();
+  console.log("Person created");
+  console.log(person);
 }).catch((e) => {
   console.log(e)
 }).finally(() => {
