5e0501defb434e46722e55306e8bd46c80ccb115
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-15 11:59:34 +0600

    create use functionality

--- 12-Attendance-System-RA/server/service/auth.js
+++ 12-Attendance-System-RA/server/service/auth.js
@@ -1,36 +1,35 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const { findUserByProperty, createNewUser } = require("./user");
 
-const registerService = async () => {
-  let user = await User.findOne({ email });
+const registerService = async ({ name, email, password }) => {
+  let user = await findUserByProperty("email", email);
   if (user) {
-    return res.status(400).json({ message: "User already exist" });
+    const error = new Error("User already exist");
+    error.status = 400;
+    throw error;
   }
-
-  user = new User({ name, email, password, accountStatus });
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
-  user.password = hash;
-
-  await user.save();
+  return createNewUser({ name, email, password: hash })
 }
 
 const loginService = async () => {
-  const user = await User.findOne({ email });
+  // const user = await User.findOne({ email });
 
-  if (!user) {
-    return res.status(400).json({ message: "Invalid Credential" });
-  }
+  // if (!user) {
+  //   return res.status(400).json({ message: "Invalid Credential" });
+  // }
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "Invalid Credential" });
-  }
+  // const isMatch = await bcrypt.compare(password, user.password);
+  // if (!isMatch) {
+  //   return res.status(400).json({ message: "Invalid Credential" });
+  // }
 
-  delete user._doc.password;
-  const token = jwt.sign(user._doc, 'secret-key', { expiresIn: "20s" });
+  // delete user._doc.password;
+  // const token = jwt.sign(user._doc, 'secret-key', { expiresIn: "20s" });
 }
 
 module.exports = {
--- 12-Attendance-System-RA/server/service/user.js
+++ 12-Attendance-System-RA/server/service/user.js
@@ -7,6 +7,12 @@ const findUserByProperty = (key, value) => {
   return User.findOne({ [key]: value });
 }
 
+const createNewUser = ({ name, email, password }) => {
+  const user = new User({ name, email, password });
+  return user.save();
+}
+
 module.exports = {
-  findUserByProperty
+  findUserByProperty,
+  createNewUser
 }
