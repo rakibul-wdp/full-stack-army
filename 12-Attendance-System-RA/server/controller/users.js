544e7a8c6b6384fdb81bfdf5eef1f7c5ca048728
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-15 15:12:42 +0600

    create users controller boilerplate

---
+++ 12-Attendance-System-RA/server/controller/users.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+
+const getUsers = (req, res, next) => {
+
+}
+
+const getUserById = (req, res, next) => {
+
+}
+
+const postUser = (req, res, next) => {
+
+}
+
+const putUserById = (req, res, next) => {
+
+}
+
+const patchUserById = (req, res, next) => {
+
+}
+
+const deleteUserById = (req, res, next) => {
+
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+  putUserById,
+  patchUserById,
+  deleteUserById
+}
