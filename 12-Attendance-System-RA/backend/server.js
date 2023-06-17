8e639ff72315f835be27191058a8b93ec5f369c3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-17 12:40:10 +0600

    create global error handler

--- 12-Attendance-System-RA/backend/server.js
+++ 12-Attendance-System-RA/backend/server.js
@@ -4,6 +4,13 @@ const connectDB = require("./db");
 const app = express();
 app.use(express.json());
 
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  const message = err.message ? err.message : "Server Error Occurred";
+  const status = err.status ? err.status : 500;
+  res.status(status).json({ message });
+})
+
 connectDB("mongodb+srv://doctor_practice:28LWDslaloONGvzu@cluster0.aee2j.mongodb.net/?retryWrites=true&w=majority").then(() => {
   console.log("Database Connected");
   app.listen(5000, () => {
