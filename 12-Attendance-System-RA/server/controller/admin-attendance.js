acd58abba7aba564649731c5ffb1a4f59f0419c5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-17 09:28:19 +0600

    enable admin functionality

--- 12-Attendance-System-RA/server/controller/admin-attendance.js
+++ 12-Attendance-System-RA/server/controller/admin-attendance.js
@@ -1,4 +1,19 @@
-const getEnable = (req, res, next) => { };
+const AdminAttendance = require("../models/AdminAttendance");
+const error = require("../utils/error");
+
+const getEnable = async (req, res, next) => {
+  try {
+    const running = await AdminAttendance.findOne({ status: "RUNNING" });
+    if (running) {
+      throw error("Already Running", 400);
+    }
+    const attendance = new AdminAttendance({});
+    await attendance.save();
+    return res.status(201).json({ message: "Success", attendance });
+  } catch (e) {
+    next(e);
+  }
+};
 
 const getDisable = (req, res, next) => { };
 
