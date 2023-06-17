411776f6a5431e075555d385dea438b47448faea
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-17 11:35:29 +0600

    create getAttendance and getAttendanceStatus

--- 12-Attendance-System-RA/server/routes/student-attendance.js
+++ 12-Attendance-System-RA/server/routes/student-attendance.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { getAttendanceStatus, getAttendance } = require("../controller/student-attendance");
 
 router.get("/status", getAttendanceStatus);
-router.get("/", getAttendance);
+router.get("/:id", getAttendance);
 
 module.exports = router;
