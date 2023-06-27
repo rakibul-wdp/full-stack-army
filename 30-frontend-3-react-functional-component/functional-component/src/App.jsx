b37e85d9c006850ea739088a0026fc52d76eda4a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-27 18:49:49 +0600

    create main tasks component

--- 30-frontend-3-react-functional-component/functional-component/src/App.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Form from './pages/Form';
 import Home from './pages/Home';
 import Tasks from './pages/Tasks';
+import MainTasks from './pages/MainTasks';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/form' element={<Form />} />
           <Route path='/tasks' element={<Tasks />} />
+          <Route path='/todo' element={<MainTasks />} />
         </Routes>
       </BrowserRouter>
     </div>
