d88dc2dba23ae061668c2d83b013eccf234aa336
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-27 15:29:23 +0600

    add create task functionality

--- 30-frontend-3-react-functional-component/functional-component/src/pages/Tasks.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/pages/Tasks.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react'
 
 const Tasks = () => {
   const [tasks, setTasks] = useState([]);
+  const [taskValue, setTaskValue] = useState("");
+
+  const handleTaskAdd = (e) => {
+    e.preventDefault();
+
+    setTasks([...tasks, { task: taskValue }])
+  }
+
+  console.log(tasks);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
@@ -9,15 +18,18 @@ const Tasks = () => {
       <div>
         <nav style={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "20px" }}>
           <h3>All Task</h3>
-          <input type="text" name="task" id="task" />
-          <h3>Add</h3>
+          <form onSubmit={handleTaskAdd}>
+            <input value={taskValue}
+              onChange={(e) => setTaskValue(e.target.value)} type="text" name="task" id="task" style={{ marginRight: "10px", padding: "5px 0", fontSize: "15px" }} />
+            <input type="submit" value="Add" style={{ fontSize: "16px", fontWeight: "bold" }} />
+          </form>
           <h3>Completed</h3>
           <h3>Incomplete</h3>
         </nav>
         <ol>
           {
-            tasks.map((task) => (
-              <li key={task.id}>{task}</li>
+            tasks.map((task, index) => (
+              <li key={index}>{task.task}</li>
             ))
           }
         </ol>
