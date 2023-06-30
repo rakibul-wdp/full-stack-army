11085289a4388ee19d8d0c8c6fcb46316538486a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-30 15:33:27 +0600

    show bread crumbs

---
+++ 30-frontend-3-react-functional-component/functional-component/src/mockData/socialIcons.js
@@ -0,0 +1,33 @@
+import { AiFillFacebook } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+
+export const socialIcons = [
+  {
+    id: 1,
+    // icon: <BsFacebook />,
+    color: "#3A589D",
+    tooltip: "Share on Facebook",
+    link: "https://facebook.com"
+  }
+]
+
+export const product = {
+  breadcrumbs: [
+    {
+      text: "Home",
+      link: "https://google.com?q=home"
+    },
+    {
+      text: "Shop",
+      link: "https://google.com?q=shop"
+    },
+    {
+      text: "Men",
+      link: "https://google.com?q=men"
+    },
+    {
+      text: "T-Shirts",
+      link: "https://google.com?q=tshirt"
+    },
+  ]
+}
--- 30-frontend-3-react-functional-component/functional-component/src/pages/ProductCard.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/pages/ProductCard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { socialIcons, product } from '../mockData/socialIcons';
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div>
+      <h3>Bread Crumbs</h3>
+      <div style={{ display: "flex", gap: "10px" }}>
+        {
+          product.breadcrumbs.map((item, index) => (
+            <div key={index}>
+              <a href={item.link}>{item.text}</a>
+              <span>{" / "}</span>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
