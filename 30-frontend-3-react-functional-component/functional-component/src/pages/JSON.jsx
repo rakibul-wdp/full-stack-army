22a47aff9c9a93d5ce3e20d6a02f57622006743c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-01 14:33:15 +0600

    get from value by state

--- 30-frontend-3-react-functional-component/functional-component/src/pages/JSON.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/pages/JSON.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const JSON = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(formState);
+  };
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    })
   }
 
   return (
@@ -11,15 +24,15 @@ const JSON = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>What is Your Name?</label>
-          <input type="text" name="name" id="name" placeholder='John Doe' />
+          <input onChange={handleChange} value={formState.name} type="text" name="name" id="name" placeholder='John Doe' />
         </div>
         <div>
           <label>What is Your Email?</label>
-          <input type="email" name="email" id="email" placeholder='example@gmail.com' />
+          <input onChange={handleChange} value={formState.email} type="email" name="email" id="email" placeholder='example@gmail.com' />
         </div>
         <div>
           <label>What is Your Phone No?</label>
-          <input type="tel" name="phone" id="phone" placeholder='+8801707453911' />
+          <input onChange={handleChange} value={formState.phone} type="tel" name="phone" id="phone" placeholder='+8801707453911' />
         </div>
         <div>
           <button type="submit">Submit</button>
