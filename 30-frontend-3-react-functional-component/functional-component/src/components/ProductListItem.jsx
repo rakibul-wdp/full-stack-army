dd4328750b5140721fe5c211410488833fec4d03
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-03 19:31:06 +0600

    product handle dynamically

--- 30-frontend-3-react-functional-component/functional-component/src/components/ProductListItem.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/components/ProductListItem.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const ProductListItem = () => {
+const ProductListItem = ({ product, stock }) => {
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
-    if (count < 10) {
+    if (count < stock) {
       setCount(count + 1);
     }
   }
@@ -17,10 +17,10 @@ const ProductListItem = () => {
 
   return (
     <div>
-      <h1>Product 1</h1>
-      <h3>{count} / 10</h3>
+      <h1>{product}</h1>
+      <h3>{count} / {stock}</h3>
       <div>
-        <button disabled={count === 10} onClick={handleIncrement}>Increment</button>
+        <button disabled={count === stock} onClick={handleIncrement}>Increment</button>
         <button disabled={count === 0} onClick={handleDecrement}>Decrement</button>
       </div>
     </div>
--- 30-frontend-3-react-functional-component/functional-component/src/pages/State.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/pages/State.jsx
@@ -5,8 +5,9 @@ const State = () => {
 
   return (
     <div>
-      <ProductListItem />
-      <ProductListItem />
+      <ProductListItem product="Keyboard" stock={10} />
+      <ProductListItem product="Mouse" stock={5} />
+      <ProductListItem product="Headphone" stock={0} />
     </div>
   )
 }
