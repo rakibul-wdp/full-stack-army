96076ce8429b1d861adc234807f7a7018bb280a3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-03 20:34:33 +0600

    create products data

---
+++ 30-frontend-3-react-functional-component/functional-component/src/mockData/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    productName: "Keyboard",
+    stock: 10,
+    price: 2000
+  },
+  {
+    id: 2,
+    productName: "Mouse",
+    stock: 5,
+    price: 1500
+  },
+  {
+    id: 3,
+    productName: "Headphone",
+    stock: 8,
+    price: 2500
+  }
+]
