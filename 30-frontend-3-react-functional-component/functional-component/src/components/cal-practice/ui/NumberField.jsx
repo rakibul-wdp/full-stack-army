e941d164de88e2b2ddca84446ec1bde69c9fd3af
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-26 07:51:12 +0600

    create number field common component

---
+++ 30-frontend-3-react-functional-component/functional-component/src/components/cal-practice/ui/NumberField.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+const NumberField = ({ value, onChange, name }) => {
+  const style = {
+    padding: "0.25rem",
+    borderRadius: "0.1rem",
+    border: "1px solid gray",
+    background: "#fff",
+    color: "#000",
+    outline: "none",
+  };
+
+  return (
+    <input
+      style={style}
+      name={name}
+      value={value}
+      onChange={onChange}
+      type="number"
+    />
+  );
+};
+
+NumberField.propTypes = {
+  value: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default NumberField;
