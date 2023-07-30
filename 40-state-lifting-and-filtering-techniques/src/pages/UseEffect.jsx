fe443c5bc795975231da2740184f93a9c7e4d84c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-30 18:07:14 +0600

    implement use effect in count

--- 40-state-lifting-and-filtering-techniques/src/pages/UseEffect.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/UseEffect.jsx
@@ -1,19 +1,22 @@
-/* eslint-disable no-undef */
-
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const UseEffect = () => {
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+
   useEffect(() => {
-    $("#btn").on("click", function () {
-      alert("hello, i'm jquery still alive");
-    });
-  });
+    if (count == 5) {
+      setLock(true);
+    }
+  }, [count]);
+  console.log("Count", count, "lock", lock);
 
   return (
     <div>
-      <h1 id="test">Hello World</h1>
-      <button id="btn">Click Me</button>
-      <div id="container"></div>
+      <h1 id="test">{count}</h1>
+      <button id="btn" disabled={lock} onClick={() => setCount(count + 1)}>
+        ADD {lock && "(locked)"}
+      </button>
     </div>
   );
 };
