e774b4b729bc97d744a45f1743c70712ae2943f0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-31 09:48:24 +0600

    load dynamic user data

--- 40-state-lifting-and-filtering-techniques/src/pages/AJAX.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/AJAX.jsx
@@ -1,22 +1,53 @@
 import { useEffect, useState } from "react";
 
 const AJAX = () => {
-  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [id, setId] = useState(1);
+
+  const max = 10;
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+      .then((data) => setUser(data))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  const nextHandler = () => {
+    if (id < max) {
+      setId(id + 1);
+    }
+  };
+
+  const prevHandler = () => {
+    if (id > 1) {
+      setId(id - 1);
+    }
+  };
 
   return (
     <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <h1>User Details - {id}</h1>
+      {loading && <p>Loading...!</p>}
+      {!loading && user && (
+        <div>
+          name: {user.name}
+          <br />
+          email: {user.email}
+          <br />
+          Phone: {user.phone}
+        </div>
+      )}
+      <div>
+        <button disabled={id == 1} onClick={prevHandler}>
+          Previous
+        </button>
+        <button disabled={id == max} onClick={nextHandler}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
