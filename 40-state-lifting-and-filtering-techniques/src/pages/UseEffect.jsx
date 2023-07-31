259d721dda230a0d1b0e171f6ef92d46e158ca1b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-31 08:08:01 +0600

    some changes

--- 40-state-lifting-and-filtering-techniques/src/pages/UseEffect.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/UseEffect.jsx
@@ -3,19 +3,33 @@ import { useEffect, useState } from "react";
 const UseEffect = () => {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
+  const [timeCount, setTimeCount] = useState(5);
 
   useEffect(() => {
     if (count == 5) {
       setLock(true);
     }
+
+    return () => {};
   }, [count]);
-  console.log("Count", count, "lock", lock);
+
+  /**
+   * 1. create a interval for timeCount
+   * 2. set count to 0, set lock to false and time count to 5
+   */
+
+  let timeInterval = null;
+  useEffect(() => {
+    timeInterval = setInterval(() => {
+      setTimeCount(timeCount - 1);
+    }, 1000);
+  }, [lock]);
 
   return (
     <div>
       <h1 id="test">{count}</h1>
       <button id="btn" disabled={lock} onClick={() => setCount(count + 1)}>
-        ADD {lock && "(locked)"}
+        ADD {lock && `(locked - ${timeCount}s)`}
       </button>
     </div>
   );
