974f73de916bdd7eb6745fb20b9e325b4dd2c023
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-31 16:40:20 +0600

    fetch data by custom hook

---
+++ 40-state-lifting-and-filtering-techniques/src/hooks/useFetchData.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetchData = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const result = await res.json();
+      setData(result);
+      setError("");
+      setLoading(false);
+    } catch (e) {
+      setError("server error occurred");
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+export default useFetchData;
--- 40-state-lifting-and-filtering-techniques/src/pages/Example.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/Example.jsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from "react";
+import useFetchData from "../hooks/useFetchData";
 
 const Example = () => {
-  const [users, setUsers] = useState([]);
-  const [userLoading, setUserLoading] = useState(false);
-  const [userError, setUserError] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [postLoading, setPostLoading] = useState(false);
-  const [postError, setPostError] = useState("");
-
-  useEffect(() => {
-    fetchUsers();
-    fetchPosts();
-  }, []);
-
-  const fetchUsers = async () => {
-    setUserLoading(true);
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await res.json();
-      setUsers(data);
-      setUserError("");
-      setUserLoading(false);
-    } catch (e) {
-      setUserError("server error occurred");
-      setUserLoading(false);
-    }
-  };
-
-  const fetchPosts = async () => {
-    setPostLoading(true);
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-      setPosts(data);
-      setPostError("");
-      setPostLoading(false);
-    } catch (e) {
-      setPostError("server error occurred");
-      setPostLoading(false);
-    }
-  };
+  const users = useFetchData("https://jsonplaceholder.typicode.com/users");
+  const posts = useFetchData("https://jsonplaceholder.typicode.com/posts");
+  const comments = useFetchData(
+    "https://jsonplaceholder.typicode.com/comments"
+  );
 
   return (
     <div
@@ -53,19 +19,28 @@ const Example = () => {
       <div>
         <h1>Users</h1>
         <hr />
-        {userLoading && <h3>Loading...!</h3>}
-        {userError && <h3>{userError}</h3>}
-        {users.map((user) => (
+        {users.loading && <h3>Loading...!</h3>}
+        {users.error && <h3>{users.error}</h3>}
+        {users.data.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </div>
       <div>
         <h1>Posts</h1>
         <hr />
-        {postLoading && <h3>Loading...!</h3>}
-        {postError && <h3>{postError}</h3>}
-        {posts.map((post) => (
+        {posts.loading && <h3>Loading...!</h3>}
+        {posts.error && <h3>{posts.error}</h3>}
+        {posts.data.map((post) => (
           <li key={post.id}>{post.title}</li>
+        ))}
+      </div>
+      <div>
+        <h1>Comments</h1>
+        <hr />
+        {comments.loading && <h3>Loading...!</h3>}
+        {comments.error && <h3>{comments.error}</h3>}
+        {comments.data.map((comment) => (
+          <li key={comment.id}>{comment.name}</li>
         ))}
       </div>
     </div>
