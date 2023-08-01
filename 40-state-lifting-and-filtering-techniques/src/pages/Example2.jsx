53f95a3a4732d26e72855534edf5d252ce5a35c3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-01 06:56:30 +0600

    keep button base

--- 40-state-lifting-and-filtering-techniques/src/pages/Example2.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/Example2.jsx
@@ -2,20 +2,18 @@ import { styled } from "styled-components";
 
 const BaseButton = styled.button`
   border: none;
+  border-radius: 0.15rem;
   outline: none;
-  background: white;
-  color: black;
-  font-size: 0.9rem;
   text-transform: uppercase;
   letter-spacing: 2px;
-  padding: 0.5rem 1rem;
+  cursor: pointer;
 `;
 
 const Example2 = () => {
   return (
     <div>
       <h1>Styled Component</h1>
-      <BaseButton>Button</BaseButton>
+      <BaseButton dark>Button</BaseButton>
     </div>
   );
 };
