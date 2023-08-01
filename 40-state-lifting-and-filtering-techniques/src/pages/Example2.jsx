7e7d3cf1ac93bf3af0364ba67e42a0a9d037f6c0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-01 08:08:04 +0600

    customize button

--- 40-state-lifting-and-filtering-techniques/src/pages/Example2.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/Example2.jsx
@@ -1,19 +1,35 @@
 import { styled } from "styled-components";
 
+const fontSizes = {
+  sm: "0.8rem",
+  md: "1rem",
+  lg: "1.2rem",
+};
+
 const BaseButton = styled.button`
   border: none;
   border-radius: 0.15rem;
   outline: none;
   text-transform: uppercase;
   letter-spacing: 2px;
+  padding: 0.5rem 1rem;
   cursor: pointer;
+  font-size: ${(props) => fontSizes[props.size] ?? "1rem"};
+`;
+
+const PrimaryButton = styled(BaseButton)`
+  background: red;
+  color: wite;
 `;
 
 const Example2 = () => {
   return (
     <div>
       <h1>Styled Component</h1>
-      <BaseButton dark>Button</BaseButton>
+      <BaseButton dark size="lg">
+        Button
+      </BaseButton>
+      <PrimaryButton size="lg">Primary Button</PrimaryButton>
     </div>
   );
 };
