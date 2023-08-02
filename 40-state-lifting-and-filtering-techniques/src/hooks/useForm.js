d8feb62b844e9527f378d6f697de39ee148839db
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-02 21:35:49 +0600

    create helper function

--- 40-state-lifting-and-filtering-techniques/src/hooks/useForm.js
+++ 40-state-lifting-and-filtering-techniques/src/hooks/useForm.js
@@ -1,10 +1,24 @@
 const params = {
-  init: 
-}
+  init: {},
+};
 
-const useForm = () => { };
+const useForm = ({ init }) => {
+  const state = mapValuesToState(init);
+  console.log(state);
+};
 
 // helper function
+const mapValuesToState = (values) => {
+  return Object.keys(values).reduce((acc, key) => {
+    acc[key] = {
+      value: values[key],
+      error: "",
+      focused: false,
+      touched: false,
+    };
 
+    return acc;
+  }, {});
+};
 
 export default useForm;
