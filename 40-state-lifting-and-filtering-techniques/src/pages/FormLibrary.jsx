c668e4223feadaa5ad4d8e3530292746a24b6f4a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-03 11:38:49 +0600

    create initial state and validate

--- 40-state-lifting-and-filtering-techniques/src/pages/FormLibrary.jsx
+++ 40-state-lifting-and-filtering-techniques/src/pages/FormLibrary.jsx
@@ -1,7 +1,36 @@
 import useForm from "../hooks/useForm";
 
+const init = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+};
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "First name is required";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Last name is required";
+  }
+
+  if (!values.email) {
+    errors.email = "Email name is required";
+  }
+
+  if (!values.password) {
+    errors.password = "Password name is required";
+  }
+
+  return errors;
+};
+
 const FormLibrary = () => {
-  const { formState } = useForm({});
+  const { formState } = useForm({ init, validate });
 
   console.log(formState);
 
