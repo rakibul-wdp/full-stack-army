cdcf9db8cf1f8baa53e0a854c74fe53ced21d77d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-03 09:35:56 +0600

    create validate of handle submit

--- 40-state-lifting-and-filtering-techniques/src/hooks/useForm.js
+++ 40-state-lifting-and-filtering-techniques/src/hooks/useForm.js
@@ -20,7 +20,7 @@ const useForm = ({ init, validate }) => {
     const oldState = deepClone(state);
     oldState[key].value = value;
 
-    const values = mapStateToValues(state);
+    const values = mapStateToKeys(oldState, "value");
     const { errors } = checkValidity(values);
 
     if (oldState[key].touched && errors[key]) {
@@ -48,7 +48,7 @@ const useForm = ({ init, validate }) => {
   const handleBlur = (e) => {
     const key = e.target.name;
 
-    const values = mapStateToValues(state);
+    const values = mapStateToKeys(state, "value");
     const { errors } = checkValidity(values);
 
     const oldState = deepClone(state);
@@ -61,6 +61,32 @@ const useForm = ({ init, validate }) => {
 
     oldState[key].focused = false;
     setState(oldState);
+  };
+
+  const handleSubmit = (e, cb) => {
+    e.preventDefault();
+
+    if (typeof validate === "boolean") {
+      if (validate) {
+        cb({
+          values: mapStateToKeys(state, "value"),
+          touched: mapStateToKeys(state, "touched"),
+          focused: mapStateToKeys(state, "focused"),
+          hasError: false,
+        });
+      }
+    }
+
+    if (isValid) {
+      console.log(state);
+    } else {
+      const oldState = deepClone(state);
+      Object.keys(errors).forEach((key) => {
+        oldState[key].error = errors[key];
+      });
+
+      setState(oldState);
+    }
   };
 
   return {
@@ -87,9 +113,9 @@ const mapValuesToState = (values) => {
   }, {});
 };
 
-const mapStateToValues = (state) => {
+const mapStateToKeys = (state, key) => {
   return Object.keys(state).reduce((acc, cur) => {
-    acc[cur] = state[cur].value;
+    acc[cur] = state[cur][key];
 
     return acc;
   }, {});
