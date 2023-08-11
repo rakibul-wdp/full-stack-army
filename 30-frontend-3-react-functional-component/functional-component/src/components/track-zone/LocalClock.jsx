6a7666c2bd7697eb49dd67a4b672e909a9b7535a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-11 06:25:00 +0600

    use clockdispaly in local clock and pass all of nessary data

--- 30-frontend-3-react-functional-component/functional-component/src/components/track-zone/LocalClock.jsx
+++ 30-frontend-3-react-functional-component/functional-component/src/components/track-zone/LocalClock.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import useClock from "../../hooks/useClock";
+import ClockDisplay from "./ClockDisplay";
 
 const LocalClock = ({ clock, updateClock }) => {
   const { date, timezone, offset } = useClock(clock.timezone, clock.offset);
@@ -12,7 +13,18 @@ const LocalClock = ({ clock, updateClock }) => {
     });
   }, [date]);
 
-  return <div>LocalClock</div>;
+  return (
+    <div>
+      {date && (
+        <ClockDisplay
+          date={date}
+          title={clock.title}
+          timezone={timezone}
+          offset={offset}
+        />
+      )}
+    </div>
+  );
 };
 
 export default LocalClock;
