a28a64ab34b372d9c246c5c6b6ef18137adcf358
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-13 10:57:04 +0600

    format time with date fns

--- 48-track-zone-project/src/components/shared/clock-display/index.jsx
+++ 48-track-zone-project/src/components/shared/clock-display/index.jsx
@@ -1,13 +1,15 @@
+import { format } from "date-fns";
+
 const ClockDisplay = ({ date, title, timezone, offset }) => {
   const offsetHr = offset / 60;
 
   return (
     <div>
       <h1>Title: {title}</h1>
-      <h3>{date.toString()}</h3>
+      <h3>{format(date, "yyyy-MM-dd hh:mm:ss aaaaa'm'")}</h3>
       <p>
         {timezone}
-        {offsetHr < 0 ? `+${Math.abs(offsetHr)}` : `-${Math.abs(offsetHr)}`}
+        {offsetHr > 0 ? `+${Math.abs(offsetHr)}` : `-${Math.abs(offsetHr)}`}
       </p>
     </div>
   );
