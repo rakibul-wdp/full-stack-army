820917eda9a4343b2ba67639d8f015c76a3ad3b5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-14 08:05:48 +0600

    create useEffect of utc and offset

--- 30-frontend-3-react-functional-component/functional-component/src/hooks/useClock.js
+++ 30-frontend-3-react-functional-component/functional-component/src/hooks/useClock.js
@@ -1,3 +1,6 @@
+import { addMinutes } from "date-fns";
+import { useEffect, useState } from "react";
+
 const TIMEZONE_OFFSET = {
   PST: -7 * 60,
   EST: -4 * 60,
@@ -6,6 +9,19 @@ const TIMEZONE_OFFSET = {
   MST: -6 * 60,
 };
 
-const useClock = (timezone, offset) => {};
+const useClock = (timezone, offset) => {
+  const [localDate, setLocalDate] = useState(null);
+  const [localOffset, setLocalOffset] = useState(0);
+  const [localTimezone, setLocalTimezone] = useState("");
+  const [utc, setUTC] = useState(null);
+
+  useEffect(() => {
+    let d = new Date();
+    const lo = d.getTimezoneOffset();
+    d = addMinutes(d, lo);
+    setUTC(d);
+    setLocalOffset(lo);
+  }, []);
+};
 
 export default useClock;
