b846a88efdecf0350d39faf8ac652901dc493413
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-26 11:05:31 +0600

    create clock list time functionality

--- 48-track-zone-project/src/components/clock-list/clock-list-item.jsx
+++ 48-track-zone-project/src/components/clock-list/clock-list-item.jsx
@@ -1,5 +1,13 @@
+import useClock from "../../hooks/useClock";
+import useTimer from "../../hooks/useTimer";
+
 const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
-  return <div>ClockListItem</div>;
+  const { date } = useClock(clock.timezone, clock.offset);
+  const timer = useTimer(date);
+
+  if (!date || !timer) return null;
+
+  return <div></div>;
 };
 
 export default ClockListItem;
