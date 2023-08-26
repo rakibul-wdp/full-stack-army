d149b1fb0deac35377b6443d77cb87804393c3df
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-26 11:10:56 +0600

    import clock display and actions and pass data

--- 48-track-zone-project/src/components/clock-list/clock-list-item.jsx
+++ 48-track-zone-project/src/components/clock-list/clock-list-item.jsx
@@ -1,5 +1,8 @@
+import { formatDistance } from "date-fns";
 import useClock from "../../hooks/useClock";
 import useTimer from "../../hooks/useTimer";
+import ClockActions from "../shared/clock-actions";
+import ClockDisplay from "../shared/clock-display";
 
 const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
   const { date } = useClock(clock.timezone, clock.offset);
@@ -7,7 +10,22 @@ const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
 
   if (!date || !timer) return null;
 
-  return <div></div>;
+  return (
+    <div>
+      <ClockDisplay
+        date={timer}
+        offset={clock.offset}
+        timezone={clock.timezone}
+        title={clock.title}
+      />
+      <h3>Time difference: {formatDistance(localClock, timer)}</h3>
+      <ClockActions
+        clock={clock}
+        updateClock={updateClock}
+        deleteClock={deleteClock}
+      />
+    </div>
+  );
 };
 
 export default ClockListItem;
