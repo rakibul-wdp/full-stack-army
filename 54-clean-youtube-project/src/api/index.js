ba94821c06c795c46a555b2aef08ac927bf14b0f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-06-15 02:39:00 +0600

    create getPlaylist functionality

--- 54-clean-youtube-project/src/api/index.js
+++ 54-clean-youtube-project/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const key = import.meta.env.VITE_API_KEY;
 
 const getPlaylistItem = async (playlistId, pageToken = "", result = []) => {
-  const URL = ``;
+  const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=${playlistId}&key=${key}&pageToken=${pageToken}`;
 
   const { data } = await axios.get(URL);
   result = [...result, ...data.items];
@@ -14,3 +14,45 @@ const getPlaylistItem = async (playlistId, pageToken = "", result = []) => {
 
   return result;
 };
+
+const getPlaylist = async (playlistId) => {
+  const URL = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${playlistId}&key=${key}`;
+
+  const { data } = await axios.get(URL);
+  let playlistItems = await getPlaylistItem(playlistId);
+
+  const {
+    title: playlistTitle,
+    description: playlistDescription,
+    thumbnails,
+    channelId,
+    channelTitle,
+  } = data.items[0].snippet;
+
+  playlistItems = playlistItems.map((item) => {
+    const {
+      title,
+      description,
+      thumbnails: { high },
+    } = item.snippet;
+
+    return {
+      title,
+      description,
+      thumbnails: high,
+      contentDetails: item.contentDetails,
+    };
+  });
+
+  return {
+    playlistId,
+    playlistTitle,
+    playlistDescription,
+    playlistThumbnail: thumbnails.default,
+    channelId,
+    channelTitle,
+    playlistItems,
+  };
+};
+
+export default getPlaylist;
