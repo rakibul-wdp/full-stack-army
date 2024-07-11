f501a7b8a74dd5b1dc05005c993395cf8f555f14
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-11 08:00:39 +0600

    create navbar and playlist form

---
+++ 54-clean-youtube-project/src/components/navbar/playlist-form/index.jsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+
+const PlaylistForm = ({ open, handleClose, getPlaylistId }) => {
+  const [state, setState] = useState("");
+
+  const handleSubmit = () => {
+    // TODO: handle url later
+    if (!state) alert("Invalid State");
+    else {
+      getPlaylistId(state);
+      setState("");
+      handleClose();
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Add Playlist</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To add a new playlist please insert the playlist id or playlist link.
+          Please make sure the link is correct. Otherwise we won't able to fetch
+          the playlist information.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Playlist ID or Link"
+          fullWidth
+          variant="standard"
+          onChange={(e) => setState(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Add Playlist</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default PlaylistForm;
