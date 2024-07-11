9e0f7a036cdc6038338e7b03fcb7c791e415815f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-11 08:35:11 +0600

    setup with react router dom in app file

--- 54-clean-youtube-project/src/App.jsx
+++ 54-clean-youtube-project/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, CssBaseline, Grid, Typography } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar";
+import PlaylistCardItem from "./components/playlist-card-item";
+import usePlaylists from "./hooks/usePlaylists";
 
-function App() {
-  const [count, setCount] = useState(0)
+const HomePage = ({ playlistArray }) => {
+  return (
+    <Container maxWidth={"lg"} sx={{ my: 16 }}>
+      {playlistArray.length > 0 && (
+        <Grid container alignItems="stretch">
+          {playlistArray.map((item) => (
+            <Grid item xs={12} md={6} lg={4} mb={2} key={item.playlistId}>
+              <PlaylistCardItem
+                key={item.playlistId}
+                playlistThumbnail={item.playlistThumbnail}
+                playlistTitle={item.playlistTitle}
+                channelTitle={item.channelTitle}
+                playlistId={item.playlistId}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Container>
+  );
+};
+
+const PlayerPage = ({ playlists }) => {
+  const { playlistId } = useParams();
+  const current = playlists[playlistId];
+  console.log("Current Course -->", current);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxWidth={"lg"} sx={{ my: 16 }}>
+      <Typography variant="h2" align="center">
+        {current.playlistTitle}
+      </Typography>
+      <Typography variant="body1">{current.playlistDescription}</Typography>
+    </Container>
+  );
+};
+
+const NotFound = () => {
+  return (
+    <Container maxWidth={"lg"} sx={{ my: 16 }}>
+      <Typography variant="h2" align="center">
+        404 Page Not Found!
+      </Typography>
+    </Container>
+  );
+};
+
+function App() {
+  const { playlists, error, getPlaylistById } = usePlaylists();
+
+  const playlistArray = Object.values(playlists);
+
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <Navbar getPlaylistById={getPlaylistById} />
+      <Routes>
+        <Route
+          path="/player/:playlistId"
+          element={<PlayerPage playlists={playlists} />}
+        />
+        <Route path="/" element={<HomePage playlistArray={playlistArray} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
