2dac5df6aa69ddf77e1aeb636e559a0e0ae62b8e
rakibul-wdp <rakibul.wdp@gmail.com>
2024-11-04 04:05:00 +0600

    add margin and button atoms

--- 81-Design-System-SCSS/scss/src/scripts/build.js
+++ 81-Design-System-SCSS/scss/src/scripts/build.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const sass = require("sass");
 
+/**
+ * compile any scss file to css
+ * @param {string} src source file;
+ * @param {string} output output file
+ */
 const compile = (src, output) => {
   const result = sass.compile(path.resolve(src), {
     style: "expanded",
@@ -11,4 +16,5 @@ const compile = (src, output) => {
   fs.writeFileSync(path.resolve(output), result.css);
 };
 
+// compile the global css
 compile("src/global.scss", "lib/global.css");
