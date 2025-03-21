a0dfba9ef138d666db493d53f206c6919aa34ace
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-21 14:29:52 +0600

    used button from library

--- 81-Design-System-SCSS/playgrounds/src/App.tsx
+++ 81-Design-System-SCSS/playgrounds/src/App.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@ds.e/react/lib";
+import "@ds.e/scss/lib/Button.css";
+
 function App() {
-  return <h1>hello monorepo</h1>;
+  return (
+    <div>
+      <h1>hello monorepo</h1>
+      <Button title="this is a button" onClick={() => alert("hello world!")}>
+        Click You
+      </Button>
+    </div>
+  );
 }
 
 export default App;
